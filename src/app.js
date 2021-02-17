78bdaf34a395d9c6e249277431e20d9b3a182669
Lucas Xavier <lucasxavier_@outlook.com.br>
2021-02-17 05:38:49 -0300

    First challenge - NodeJS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4"); //generate unique id
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -12,14 +13,27 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+
+  
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+
+  const {title, url, techs} = request.body;
+
+  const project = { id: uuid(), title , url, techs, likes: 0 };
+
+        repositories.push(project);
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+
+  const { id }
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
